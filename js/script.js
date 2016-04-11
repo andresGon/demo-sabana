4a907ffe7fb386ef436fb58c99a77ba9c37fb698
Andrés González <miroenlared@gmail.com>
2016-04-11 16:58:28 -0500

    loop nave 02

--- js/script.js
+++ js/script.js
@@ -7,24 +7,102 @@ var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "
 // 				//.addIndicators()
 // 				.addTo(controller);
 
+// ===================================== section02 ===================================
 new ScrollMagic.Scene({triggerElement: ".sec02"})
-				.setTween(".sec02 > div", {y: "80%", ease: Linear.easeNone})
-				//.addIndicators()
+				.setTween(".sec02 .child02", {y: "80%", ease: Linear.easeNone})
+				.addIndicators()
 				.addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".cont01"})
+				.setTween(".nube01", 1,  { left: 250 } )
+				.addIndicators()
+				.addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".cont02"})
+				.setTween(".nube03", { left: 150, opacity: 1} )
+				.addIndicators()
+				.addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".cont02"})
+				.setTween(".nube02", { right: 150, opacity: 1} )
+				.addIndicators()
+				.addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".cont02"})
+				.setTween(".nube04", { right: 350} )
+				.addIndicators()
+				.addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".cont02"})
+				.setTween(".nube05", { bottom: 150} )
+				.addIndicators()
+				.addTo(controller);
+new ScrollMagic.Scene({triggerElement: ".cont02"})
+				.setTween(".nube06", { bottom: 150} )
+				.addIndicators()
+				.addTo(controller);
+
+
+				$(function () { // wait for document ready
+						var flightpath = {
+							entry : {
+								curviness: 1.25,
+								autoRotate: true,
+								values: [
+										{x: 100,	y: -20},
+										{x: 300,	y: 10}
+									]
+							},
+							looping : {
+								curviness: 1.25,
+								autoRotate: true,
+								values: [
+										{x: 510,	y: 60},
+										{x: 620,	y: -60},
+										{x: 500,	y: -100},
+										{x: 380,	y: 20},
+										{x: 500,	y: 60},
+										{x: 580,	y: 20},
+										{x: 620,	y: 15}
+									]
+							},
+							leave : {
+								curviness: 1.25,
+								autoRotate: true,
+								values: [
+										{x: 660,	y: 20},
+										{x: 800,	y: 130},
+										{x: $(window).width() + 300,	y: -100},
+									]
+							}
+						};
+						// init controller
+						var controller = new ScrollMagic.Controller();
+
+						// create tween
+						var tween = new TimelineMax()
+							.add(TweenMax.to($(".nave"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
+							.add(TweenMax.to($(".nave"), 2, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
+							.add(TweenMax.to($(".nave"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
+
+						// build scene
+						var scene = new ScrollMagic.Scene({triggerElement: ".cont02", duration: 500, offset: 100})
+										.setPin(".wrapNave")
+										.setTween(tween)
+										.addIndicators() // add indicators (requires plugin)
+										.addTo(controller);
+					})
+
+// ===================================== section03 ===================================
 
 new ScrollMagic.Scene({triggerElement: ".sec03"})
 				.setTween(".sec03 > div", {y: "80%", ease: Linear.easeNone})
-				//.addIndicators()
+				.addIndicators()
 				.addTo(controller);
 new ScrollMagic.Scene({triggerElement: ".sec04"})
 				.setTween(".sec04 > div", {y: "80%", ease: Linear.easeNone})
-				//.addIndicators()
+				.addIndicators()
 				.addTo(controller);
 new ScrollMagic.Scene({triggerElement: ".sec05"})
 				.setTween(".sec05 > div", {y: "80%", ease: Linear.easeNone})
-				//.addIndicators()
+				.addIndicators()
 				.addTo(controller);
 new ScrollMagic.Scene({triggerElement: ".sec06"})
 				.setTween(".sec06 > div", {y: "80%", ease: Linear.easeNone})
-				//.addIndicators()
+				.addIndicators()
 				.addTo(controller);
